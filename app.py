e09e2a53d7ab90a3b87aadae5c045e97fdff6381
sennrr <testneteth70@gmail.com>
2025-02-10 04:30:08 +0100

    Fix merge conflict in app.py

--- app.py
+++ app.py
@@ -137,7 +137,6 @@ with app.app_context():
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=8000, debug=True)
-=======
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
